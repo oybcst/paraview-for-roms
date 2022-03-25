dac15141d4fb051e3071b1afd0de72f81d1d0686
mvanmoer <mvanmoer@illinois.edu>
2022-03-25 10:30:40 -0500

    Changed to take /path/to/mesh, /path/to/data as args, encapsulated into function

--- merge_netcdfs/combine_grid_data.py
+++ merge_netcdfs/combine_grid_data.py
@@ -11,63 +11,79 @@
 # are 1D dimensions AND variables. We want this so that we can then
 # in PV or VisIt convert degrees to meters to match the velocity units.
 
+# We assume that the lat, lon, depth and data are all at the rho pts
+# and have the same dimensions.
+
+# We assume the data is ROMS output. This script has not been tested
+# with data from other simulation packages.
+
 import netCDF4
+import argparse
+import sys
 
-# note that netCDF4.Dataset() doesn't support tilde expansion for $HOME
-datapath = './'
-outpath = './'
-gridfile = 'mobile_whole_grid05032021.nc'
-datafile = 'roms_mobile_bay_output.nc'
+def merge(meshfile, datafile):
 
-# the grid data
-grid = netCDF4.Dataset(datapath+gridfile, 'r', format='NETCDF4')
+    mesh = netCDF4.Dataset(meshfile, 'r', format='NETCDF4')
 
-# get unique lons
-lons = grid['lon_rho'][0,:]
+    # get unique lons
+    lons = mesh['lon_rho'][0,:]
 
-# get unique lats
-lats = grid['lat_rho'][:,0]
+    # get unique lats
+    lats = mesh['lat_rho'][:,0]
 
-# the actual data 
-romsdata = netCDF4.Dataset(datapath+datafile, 'r', format='NETCDF4')
+    # the actual data 
+    data = netCDF4.Dataset(datafile, 'r', format='NETCDF4')
 
-# the merged output
-merged = netCDF4.Dataset(outpath+'merged.nc', 'w', format='NETCDF4')
+    # the merged output
+    merged = netCDF4.Dataset('./merged.nc', 'w', format='NETCDF4')
 
-# createDimension(name, size) - size=None means unlimited
-# replaces xi_rho
-merged.createDimension('lon_rho', lons.size)
-# replaces eta_rho
-merged.createDimension('lat_rho', lats.size)
-merged.createDimension('ocean_time', None)
-merged.createDimension('s_rho', None)
+    # createDimension(name, size) - size=None means unlimited
+    # replaces xi_rho
+    merged.createDimension('lon_rho', lons.size)
+    # replaces eta_rho
+    merged.createDimension('lat_rho', lats.size)
+    merged.createDimension('ocean_time', None)
+    merged.createDimension('s_rho', None)
 
-# copy variables from romsdata to merged... but not all, just the
-# data which has dims (ocean_time, s_rho, 840, 600)
-# Note that there is no w_rho, so there still won't be a 3D velocity
-# vector, just the vector in the uv plane at each rho point
-wantedshape = tuple([romsdata['ocean_time'].size,
-                   romsdata['s_rho'].size,
+    # copy variables from data to merged... but not all, just the
+    # data which has dims (ocean_time, s_rho, 840, 600)
+    # Note that there is no w_rho, so there still won't be a 3D velocity
+    # vector, just the vector in the uv plane at each rho point
+    wantedshape = tuple([data['ocean_time'].size,
+                   data['s_rho'].size,
                    lats.size,
                    lons.size])
 
-for name, var in romsdata.variables.items():
-    if var.shape == wantedshape:
-        merged.createVariable(name, var.dtype,
+    for name, var in data.variables.items():
+        if var.shape == wantedshape:
+            merged.createVariable(name, var.dtype,
                 ('ocean_time', 's_rho', 'lat_rho', 'lon_rho'))
-        # copy the attrs
-        merged[name].setncatts(romsdata[name].__dict__)
-        # copy the data
-        merged[name][:] = romsdata[name][:]
+            # copy the attrs
+            merged[name].setncatts(data[name].__dict__)
+            # copy the data
+            merged[name][:] = data[name][:]
 
-# copy lon, lat vars from grid, but just as the already extracted
-# 1D lons and lats vars
-merged.createVariable('lon_rho', grid['lon_rho'].dtype, ('lon_rho'))
-merged['lon_rho'][:] = lons
+    # copy lon, lat vars from mesh, but just as the already extracted
+    # 1D lons and lats vars
+    merged.createVariable('lon_rho', mesh['lon_rho'].dtype, ('lon_rho'))
+    merged['lon_rho'][:] = lons
 
-merged.createVariable('lat_rho', grid['lat_rho'].dtype, ('lat_rho'))
-merged['lat_rho'][:] = lats
+    merged.createVariable('lat_rho', mesh['lat_rho'].dtype, ('lat_rho'))
+    merged['lat_rho'][:] = lats
 
-grid.close()
-romsdata.close()
-merged.close()
+    mesh.close()
+    data.close()
+    merged.close()
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='''merge NetCDF file with lon,lat,
+            depth to NetCDF file with data, assumes all on rho-points''')
+    parser.add_argument('meshfile', help='path to NetCDF file with lon, lat, depth')
+    parser.add_argument('datafile', help='path to NetCDF file with data')
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+
+    merge(args.meshfile, args.datafile)
