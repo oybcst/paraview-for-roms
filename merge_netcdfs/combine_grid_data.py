f55a117c39bd59f0399f52dd444eeb227b373f48
Mark Van Moer <mvanmoer@illinois.edu>
2022-03-29 14:21:16 -0500

    Copying lat_rho, lon_rho, s_rho variables directly
    Prior I had forgotten to copy s_rho at all and was trying to be clever
    and save space by copying lat_rho, lon_rho as 1D variables, but decided
    to just copy them over as-is

--- merge_netcdfs/combine_grid_data.py
+++ merge_netcdfs/combine_grid_data.py
@@ -25,38 +25,32 @@ def merge(meshfile, datafile):
 
     mesh = netCDF4.Dataset(meshfile, 'r', format='NETCDF4')
 
-    # get unique lons
-    lons = mesh['lon_rho'][0,:]
-
-    # get unique lats
-    lats = mesh['lat_rho'][:,0]
-
     # the actual data 
     data = netCDF4.Dataset(datafile, 'r', format='NETCDF4')
     
     # the merged output
     merged = netCDF4.Dataset('./merged.nc', 'w', format='NETCDF4')
 
-    # replaces xi_rho
-    merged.createDimension('lon_rho', data.dimensions['xi_rho'].size)
-    # replaces eta_rho
-    merged.createDimension('lat_rho', data.dimensions['eta_rho'].size)
     merged.createDimension('s_rho', data.dimensions['s_rho'].size)
+    merged.createDimension('eta_rho', data.dimensions['eta_rho'].size)
+    merged.createDimension('xi_rho', data.dimensions['xi_rho'].size)
+    # doing a straight copy gives the current size (12) not UNLIMITED, which
+    # causes problems when being read by a vis app.
     merged.createDimension('ocean_time', None)
 
     # copy variables from data to merged... but not all, just the
     # data which has dims (ocean_time, s_rho, 840, 600)
     # Note that there is no w_rho, so there still won't be a 3D velocity
     # vector, just the vector in the uv plane at each rho point
-    wantedshape = tuple([data['ocean_time'].size,
-                   data['s_rho'].size,
-                   lats.size,
-                   lons.size])
+    wantedshape = tuple([data.dimensions['ocean_time'].size,
+                   data.dimensions['s_rho'].size,
+                   data.dimensions['eta_rho'].size,
+                   data.dimensions['xi_rho'].size])
 
     for name, var in data.variables.items():
         if var.shape == wantedshape:
             merged.createVariable(name, var.dtype,
-                ('ocean_time', 's_rho', 'lat_rho', 'lon_rho'))
+                ('ocean_time', 's_rho', 'eta_rho', 'xi_rho'))
             # copy the attrs
             merged[name].setncatts(data[name].__dict__)
             # copy the data
@@ -73,18 +67,22 @@ def merge(meshfile, datafile):
     # loss of precision
     merged['ocean_time'][:] = [x - min(merged['ocean_time'][:]) for x in merged['ocean_time'][:]] 
 
-
-    # copy lon, lat vars from mesh, but just as the already extracted
+    # copy lon, lat vars from mesh, but these are (eta_rho, xi_rho) size
     # 1D lons and lats vars. In the Mobile Bay mesh these are 2D variables,
     # it is unclear if they should also be so in the merged file. It appears
     # to be topologically orthogonal...
-    merged.createVariable('lon_rho', mesh['lon_rho'].dtype, ('lon_rho'))
+    merged.createVariable('lon_rho', mesh['lon_rho'].dtype, ('eta_rho', 'xi_rho'))
     merged['lon_rho'].setncatts(mesh['lon_rho'].__dict__)
-    merged['lon_rho'][:] = lons
+    merged['lon_rho'][:] = mesh['lon_rho'][:]
 
-    merged.createVariable('lat_rho', mesh['lat_rho'].dtype, ('lat_rho'))
+    merged.createVariable('lat_rho', mesh['lat_rho'].dtype, ('eta_rho', 'xi_rho'))
     merged['lat_rho'].setncatts(mesh['lat_rho'].__dict__)
-    merged['lat_rho'][:] = lats
+    merged['lat_rho'][:] = mesh['lat_rho'][:]
+
+    # copy s_rho var from data
+    merged.createVariable('s_rho', data['s_rho'].dtype, ('s_rho'))
+    merged['s_rho'].setncatts(data['s_rho'].__dict__)
+    merged['s_rho'][:] = data['s_rho'][:]
 
     mesh.close()
     data.close()
