531b19da9660a2437f1a65be959113b3eb9d6d30
Mark Van Moer <mvanmoer@illinois.edu>
2022-08-10 16:36:15 -0500

    renders all timesteps of a single scalar in a single NetCDF file

--- pvbatch/from-scratch.py
+++ pvbatch/from-scratch.py
@@ -1,74 +1,85 @@
-from paraview.simple import *
 from datetime import date
 import sys
 
+# There's a bug not fixed until PV 5.9.0 where argv needs to be parsed 
+# before the paraview.simple import
+# would rather have these in a YAML or JSON file...
+scalar = sys.argv[1]
+scalarMin = float(sys.argv[2])
+scalarMax = float(sys.argv[3])
+colormap = sys.argv[4]
+pngbase = sys.argv[5]
+ncfile = sys.argv[6]
+
+from paraview.simple import *
 paraview.simple._DisableFirstRenderCameraReset()
 
-#projpath = '/home/mvanmoer/Vis/projects/lowe-esrt/'
-#ncpath = projpath + 'data/derived/expanse_download/'
-#ncfile = ncpath + 'merged-20190601.nc'
-
-ncfile = sys.argv[1]
+winW = 1920 // 3
+winH = 1080
 
 YYYYMMDD = ncfile[-11:-3]
-# hours since start of ROMS data
+# hours since start of ROMS data which is 2018-11-01
 start = date(2018, 11, 1)
 curr =  date(int(YYYYMMDD[:4]),int(YYYYMMDD[4:6]),int(YYYYMMDD[6:]))
 dt = curr - start
 dthrs = dt.days * 24
 
-
+# Data is on the rho points
 nc = NetCDFReader(FileName=[ncfile])
 nc.Dimensions = '(s_rho, eta_rho, xi_rho)'
-nc.SphericalCoordinates = 0
-nc.ReplaceFillValueWithNan = 1
+nc.SphericalCoordinates = 0    # don't use spherical coords
+nc.ReplaceFillValueWithNan = 1 # DO replace fill (-9999) with NaN 
 nc.OutputType = 'Automatic'
 
+# Convert from lat, lon to meters. s_rho is meters, this
+# constant was found via googling.
+# the longitude range is -88.4987 to -87.6684
+# the latitude range is 29.7902 to 31.088
+deg2mfactor = 111139.0
 latlon2m = Transform(Input=nc)
 latlon2m.Transform = 'Transform'
-latlon2m.Transform.Scale = [111139.0, 111139.0, 1.0]
+latlon2m.Transform.Scale = [deg2mfactor, deg2mfactor, 1.0]
 
 renderView = GetActiveViewOrCreate('RenderView')
-renderView.ViewSize = [660, 1080]
+renderView.ViewSize = [winW, winH]
+renderView.OrientationAxesVisibility = 0
 
 ncDisplay = GetDisplayProperties(latlon2m, view=renderView)
 ncDisplay.Representation = 'Surface'
-ncDisplay.SetScaleArray = ['POINTS', 'oxyg']
+ncDisplay.SetScaleArray = ['POINTS', scalar]
 
-oxygLUT = GetColorTransferFunction('oxyg')
-oxygLUT.ApplyPreset('Black, Blue and White', True)
-oxygLUT.NanOpacity = 0.0
+LUT = GetColorTransferFunction(scalar)
+LUT.ApplyPreset(colormap, True)
+LUT.NanOpacity = 0.0
 
-ColorBy(ncDisplay, ('POINTS', 'oxyg'))
+ColorBy(ncDisplay, ('POINTS', scalar))
 
 # WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # this MUST be called after ColorBy(), despite what the python trace does
 # otherwise it will have NO effect!
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-oxygLUT.RescaleTransferFunction(0.0, 500.0)
+LUT.RescaleTransferFunction(scalarMin, scalarMax)
 
 # for troubleshooting
 #ncDisplay.SetScalarBarVisibility(renderView, True)
 
 # might be better to have this in lat, lon degrees and then convert to
 # meters.
-renderView.CameraPosition = [-9789519.81600239, 3382974.081183975, 353675.63746335817]
-renderView.CameraFocalPoint = [-9789519.81600239, 3382974.08113975, -81642.97223371016]
+# by inspection in ParaView
+#renderView.CameraPosition = [-9789519.81600239, 3382974.081183975, 353675.63746335817]
+#renderView.CameraFocalPoint = [-9789519.81600239, 3382974.08113975, -81642.97223371016]
+xmin,xmax,ymin,ymax,zmin,zmax = latlon2m.GetDataInformation().GetBounds()
+renderView.CameraPosition = [(xmin+xmax)/2.0, (ymin+ymax)/2.0, 300 * 1000]
+renderView.CameraFocalPoint = [(xmin+xmax)/2.0, (ymin+ymax)/2.0, -100]
 
+# Using SaveScreenshot because SaveAnimation doesn't honor the 
+# RescaleTransferFunction call!
 # 24 hrs in a day, one day per file.
 for i in range(24):
     times = nc.TimestepValues
     view = GetActiveView()
     view.ViewTime = times[i]
     Render()
-    pngfile = 'parallel-test.{:04d}.png'.format(dthrs + i)
+    pngfile = '{}.{:04d}.png'.format(pngbase, dthrs + i)
     print(f'Saving: {pngfile}...')
-    SaveScreenshot(pngfile, renderView, ImageResolution=[660, 1080])
-
-
-# SaveAnimation doesn't honor the RescaleTransferFunction call!
-#SaveAnimation('./from-scratch-test.png',
-#        renderView,
-#        ImageResolution=[660, 1080],
-#        FrameWindow=[0,23],
-#        SuffixFormat='.%04d')
+    SaveScreenshot(pngfile, renderView, ImageResolution=[winW, winH])
