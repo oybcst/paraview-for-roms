cf28a36ce7c0fdc21f23c940bdb2e40f663dbeaa
Mark Van Moer <mvanmoer@illinois.edu>
2022-08-12 16:09:14 -0500

    Added arg for z slice level

--- render_scalars/serial-render.sh
+++ render_scalars/serial-render.sh
@@ -10,7 +10,7 @@ readarray -d '' NCS < <(find $NCDIR -name "*.nc" -print0)
 renderscript=render-scalar.py
 for nc in ${NCS[@]}; do
     echo "rendering $nc"
-    #pvbatch $renderscript oxyg 0 500 'Black, Blue and White' oxygen $nc
-    #pvbatch $renderscript temp 10 35 Plasma temperature $nc
-    pvbatch $renderscript salt 0 40 'Viridis (matplotlib)' salt $nc
+    #pvbatch $renderscript oxyg 0 0 500 'Black, Blue and White' oxygen $nc
+    #pvbatch $renderscript temp 15 10 35 Plasma temperature $nc
+    pvbatch $renderscript salt 0 0 40 'Viridis (matplotlib)' salt $nc
 done
