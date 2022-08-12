f3174ff729ec68834e28faf2b639e8d92d2edcc1
Mark Van Moer <mvanmoer@illinois.edu>
2022-08-12 16:09:45 -0500

    Added calls for different colormaps for temp

--- render_scalars/parallel-render.sh
+++ render_scalars/parallel-render.sh
@@ -10,12 +10,19 @@ NCDIR=/home/mvanmoer/Vis/projects/lowe-esrt/data/derived/expanse_download/
 # get all the .nc's as a spaced-delimited list for piping into parallel
 readarray -d '' NCS < <(find $NCDIR -name "*.nc" -print0)
 
-# eventually want to have this args in an config file instead.
-renderscript="render-scalar.py salt 0 40 \"Viridis (matplotlib)\" salt"
-#renderscript="render-scalar.py oxyg 0 500 \"Black, Blue and White\" oxygen"
-#renderscript="render-scalar.py temp 10 35 Plasma temperature"
-
 parallel="time -p parallel --delay 0.2 -j $SLURM_NTASKS --joblog rendering.log --resume"
 
+# eventually want to have this args in an config file instead.
+#renderscript="render-scalar.py salt 0 0 40 \"Viridis (matplotlib)\" salt"
+renderscript="render-scalar.py oxyg 0 0 500 \"Black, Blue and White\" oxygen"
+#renderscript="render-scalar.py temp 15 10 35 Plasma temperature-plasma"
+#renderscript="render-scalar.py temp 15 10 35 \"Cool to Warm\" temperature-cool-to-warm"
+#renderscript="render-scalar.py temp 15 10 35 \"Cold and Hot\" temperature-cold-and-hot"
 
+# this one outputs Plasma?!
+#renderscript="render-scalar.py temp 15 10 35 \"Cool to Warm \(Extended\)\" temperature-cool-to-warm-extended"
+
+#renderscript="render-scalar.py temp 15 10 35 \"Spectral_lowBlue\" temperature-spectral-lowblue"
+
+#renderscript="render-scalar.py temp 15 10 35 \"Rainbow Uniform\" temperature-rainbow-uniform"
 printf '%s\n' "${NCS[@]}" | $parallel "pvbatch $renderscript {} > render.log.{#}"
